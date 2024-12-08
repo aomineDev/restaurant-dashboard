81de0b76c7af7a49ef81bfa2e7d8824af5f9bd5f
aomine <aomine1745@gmail.com>
2024-12-08 17:52:02 -0500

    feature: validate user and password field

--- src/main/java/aomine/controller/login/LoginController.java
+++ src/main/java/aomine/controller/login/LoginController.java
@@ -2,11 +2,14 @@ package aomine.controller.login;
 
 import java.awt.event.ActionEvent;
 
+import org.postgresql.translation.messages_pt_BR;
+
 import aomine.ViewManager;
 import aomine.dao.EmployeeDAO;
 import aomine.dao.RoleDAO;
 import aomine.model.Employee;
 import aomine.model.Role;
+import aomine.utils.Validate;
 import aomine.view.admin.Test;
 import aomine.view.login.LoginView;
 import at.favre.lib.crypto.bcrypt.BCrypt;
@@ -16,9 +19,11 @@ public class LoginController {
   private LoginView view;
   private RoleDAO roleDAO;
   private EmployeeDAO employeeDAO;
+  private Validate validate;
 
   public LoginController(LoginView view) {
     this.view = view;
+    validate = new Validate();
     roleDAO = new RoleDAO();
     employeeDAO = new EmployeeDAO();
 
@@ -27,13 +32,26 @@ public class LoginController {
   public void handleLoginClick(ActionEvent evt) {
     if (employeeDAO.isEmpty()) createAdmin();
 
-    MessageAlerts.getInstance().showMessage(
-      // "Error", 
-      // "Usuario y/o contraseña incorrectos.",
-      "OK!", 
-      "Usuario creado correctamente. " + String.valueOf(view.getPfPassword().getPassword()),
-      MessageAlerts.MessageType.SUCCESS
-    );
+    String username = view.getTfUsername().getText();
+
+    validate.setElement(username)
+      .isRequired("El usuario es requerido");
+
+    if (!validate.getIsValid()) {
+      errorMessage(validate.getMessage());
+      return;
+    }
+
+    String password = String.valueOf(view.getPfPassword().getPassword());
+
+    validate.setElement(password)
+      .isRequired("La contraseña es requedia")
+      .minLength("La contraseña tiene que tener mas de 8 cara cteres", 8);
+
+    if (!validate.getIsValid()) {
+      errorMessage(validate.getMessage());
+      return;
+    }
 
     // ViewManager.showView(new Test());
     // ViewManager.login();
@@ -57,5 +75,13 @@ public class LoginController {
     user.setDni(13246578);
 
     employeeDAO.add(user);
+  }
+
+  public void errorMessage(String msg) {
+    MessageAlerts.getInstance().showMessage(
+      "Error!", 
+      msg,
+      MessageAlerts.MessageType.ERROR
+    );
   }
 }
